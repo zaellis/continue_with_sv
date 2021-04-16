9aa1a7d643aaccb6c0f57b8d04b581f4733dbb37
Dawson <moore800@purdue.edu>
2021-04-16 02:32:07 -0400

    Change example and makefile

--- examples/alchitry_cu_counter/cu_top.sv
+++ examples/alchitry_cu_counter/cu_top.sv
@@ -1,31 +1,18 @@
-module top(input clk, output D1, output D2, output D3, output D4, output D5);
-   
-   reg ready = 0;
-   reg [23:0]     divider;
-   reg [3:0]      rot;
-   
-   always @(posedge clk) begin
-      if (ready) 
-        begin
-           if (divider == 12000000) 
-             begin
-                divider <= 0;
-                rot <= {rot[2:0], rot[3]};
-             end
-           else 
-             divider <= divider + 1;
-        end
-      else 
-        begin
-           ready <= 1;
-           rot <= 4'b0001;
-           divider <= 0;
-        end
+`timescale 1ns/1ps
+
+module top
+(
+   input wire clk,                     // Clock for the design
+   input wire n_rst,                   // Reset button (used for counting here)
+   output reg [7:0] led,               // User controllable LEDs
+   input wire usb_rx,                  // FTDI RX pin
+   output wire usb_tx                  // FTDI TX pin
+);
+
+   assign usb_tx = usb_rx;             // Mirror input to output
+
+   always @ (posedge n_rst) begin      // "Increment" LEDs on rising edge of n_rst
+      led = led + 1;                   // Do the increment
    end
-   
-   assign D1 = rot[0];
-   assign D2 = rot[1];
-   assign D3 = rot[2];
-   assign D4 = rot[3];
-   assign D5 = 1;
-endmodule // top
+
+endmodule
